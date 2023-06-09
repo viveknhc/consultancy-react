767123c292f4dad392c14889e9a8e2addacb8ff9
viveknhc <viveknhc@gmail.com>
2023-06-09 15:00:06 +0530

    09/06/23

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import About from './pages/About';
 import Home from './pages/Home';
 import Users from './pages/Users';
 import Header from './components/Header';
-
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Details from './pages/Details';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
@@ -20,7 +19,7 @@ function App() {
         <Route path='/' element={<Home />}> </Route>
         <Route path='about' element={<About />}> </Route>
         <Route path='users' element={<Users />}> </Route>
-        <Route path='details/:userId' element={<Details />}> </Route>
+        <Route path="details/:userId" element={<Details />}> </Route>
       </Routes>
     </BrowserRouter>
 
